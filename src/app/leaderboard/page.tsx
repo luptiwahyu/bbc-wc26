35f2230bab4c8b75e5ac8588f815f6a618504342
Lupti Wahyu <luptiwahyu@ymail.com>
2026-06-30 02:10:14 +0700

    feat: add suspense to leaderboard page

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -16,6 +16,7 @@ import { appStorage } from '@/shared/lib/utils'
 import { LogOutIcon, SwordsIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function Leaderboard() {
   const { data: leaderboards, isPending, isSuccess } = useLeaderboard()
@@ -32,65 +33,71 @@ export default function Leaderboard() {
   }
 
   return (
-    <AppContent>
-      <FieldLabel>
-        <Link href="/match" className="hover:underline">
-          <div className="flex space-x-1">
-            <span>Lihat Pertandingan</span>
-            <SwordsIcon size={14} />
-          </div>
-        </Link>
-        {logoutVisible && (
-          <>
-            <Separator orientation="vertical" />
-            <div className="cursor-pointer hover:underline" onClick={logout}>
-              <div className="flex space-x-1">
-                <span>Logout</span>
-                <LogOutIcon size={14} />
-              </div>
+    <Suspense>
+      <AppContent>
+        <FieldLabel>
+          <Link href="/match" className="hover:underline">
+            <div className="flex space-x-1">
+              <span>Lihat Pertandingan</span>
+              <SwordsIcon size={14} />
             </div>
-          </>
-        )}
-      </FieldLabel>
+          </Link>
+          {logoutVisible && (
+            <>
+              <Separator orientation="vertical" />
+              <div className="cursor-pointer hover:underline" onClick={logout}>
+                <div className="flex space-x-1">
+                  <span>Logout</span>
+                  <LogOutIcon size={14} />
+                </div>
+              </div>
+            </>
+          )}
+        </FieldLabel>
 
-      <div className="overflow-hidden rounded-sm border">
-        <Table>
-          <TableHeader className="bg-muted/50">
-            <TableRow>
-              <TableHead className="font-normal">Pemain</TableHead>
-              <TableHead className="font-normal text-center">Main</TableHead>
-              <TableHead className="font-normal text-center">Menang</TableHead>
-              <TableHead className="font-normal text-center">Kalah</TableHead>
-              <TableHead className="font-bold text-center">Poin</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {isPending && (
+        <div className="overflow-hidden rounded-sm border">
+          <Table>
+            <TableHeader className="bg-muted/50">
               <TableRow>
-                <TableCell colSpan={5} className="text-center">
-                  Loading...
-                </TableCell>
+                <TableHead className="font-normal">Pemain</TableHead>
+                <TableHead className="font-normal text-center">Main</TableHead>
+                <TableHead className="font-normal text-center">
+                  Menang
+                </TableHead>
+                <TableHead className="font-normal text-center">Kalah</TableHead>
+                <TableHead className="font-bold text-center">Poin</TableHead>
               </TableRow>
-            )}
-
-            {isSuccess &&
-              leaderboards.map((player, playerIdx) => (
-                <TableRow
-                  key={player.player_id}
-                  className={playerIdx % 2 === 1 ? 'bg-muted/50' : ''}
-                >
-                  <TableCell>{player.player_name}</TableCell>
-                  <TableCell className="text-center">{player.played}</TableCell>
-                  <TableCell className="text-center">{player.win}</TableCell>
-                  <TableCell className="text-center">{player.lose}</TableCell>
-                  <TableCell className="text-center font-bold">
-                    {player.points}
+            </TableHeader>
+            <TableBody>
+              {isPending && (
+                <TableRow>
+                  <TableCell colSpan={5} className="text-center">
+                    Loading...
                   </TableCell>
                 </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </div>
-    </AppContent>
+              )}
+
+              {isSuccess &&
+                leaderboards.map((player, playerIdx) => (
+                  <TableRow
+                    key={player.player_id}
+                    className={playerIdx % 2 === 1 ? 'bg-muted/50' : ''}
+                  >
+                    <TableCell>{player.player_name}</TableCell>
+                    <TableCell className="text-center">
+                      {player.played}
+                    </TableCell>
+                    <TableCell className="text-center">{player.win}</TableCell>
+                    <TableCell className="text-center">{player.lose}</TableCell>
+                    <TableCell className="text-center font-bold">
+                      {player.points}
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </div>
+      </AppContent>
+    </Suspense>
   )
 }
