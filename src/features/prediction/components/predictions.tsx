2c56d82131dddd8f94f1eaba1566bf977a2be682
Lupti Wahyu <luptiwahyu@ymail.com>
2026-07-16 11:32:28 +0700

    Hide prediction view until the game (#5)

--- src/features/prediction/components/predictions.tsx
+++ src/features/prediction/components/predictions.tsx
@@ -53,66 +53,80 @@ const Predictions: FC = () => {
                       </Button>
                     </CollapsibleTrigger>
                     <CollapsibleContent className="pl-[25px]">
-                      <Table className="my-1">
-                        <TableHeader>
-                          <TableRow>
-                            <TableHead className="h-6 pl-0">Prediksi</TableHead>
-                            <TableHead className="h-6 pl-0">Jawaban</TableHead>
-                          </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                          {!!prediction.predicted_winner_name && (
+                      {new Date() < new Date(prediction.match.date!) && (
+                        <Label className="my-2.5 font-normal">
+                          Bisa dilihat saat pertandingan dimulai
+                        </Label>
+                      )}
+
+                      {new Date() >= new Date(prediction.match.date!) && (
+                        <Table className="my-1">
+                          <TableHeader>
                             <TableRow>
-                              <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
-                                Pemenang
-                              </TableCell>
-                              <TableCell className="py-1 pl-0">
-                                {prediction.predicted_winner_name}
-                              </TableCell>
+                              <TableHead className="h-6 pl-0">
+                                Prediksi
+                              </TableHead>
+                              <TableHead className="h-6 pl-0">
+                                Jawaban
+                              </TableHead>
                             </TableRow>
-                          )}
-                          {prediction.predicted_total_goals !== null && (
-                            <TableRow>
-                              <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
-                                Jumlah Gol
-                              </TableCell>
-                              <TableCell className="py-1 pl-0">
-                                {prediction.predicted_total_goals}
-                              </TableCell>
-                            </TableRow>
-                          )}
-                          {!!prediction.predicted_score && (
-                            <TableRow>
-                              <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
-                                Skor
-                              </TableCell>
-                              <TableCell className="py-1 pl-0">
-                                {prediction.predicted_score}
-                              </TableCell>
-                            </TableRow>
-                          )}
-                          {!!prediction.predicted_first_team_to_score_name && (
-                            <TableRow>
-                              <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
-                                Gol Pertama (Negara)
-                              </TableCell>
-                              <TableCell className="py-1 pl-0">
-                                {prediction.predicted_first_team_to_score_name}
-                              </TableCell>
-                            </TableRow>
-                          )}
-                          {!!prediction.predicted_first_player_to_score && (
-                            <TableRow>
-                              <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
-                                Gol Pertama (Pemain)
-                              </TableCell>
-                              <TableCell className="py-1 pl-0">
-                                {prediction.predicted_first_player_to_score}
-                              </TableCell>
-                            </TableRow>
-                          )}
-                        </TableBody>
-                      </Table>
+                          </TableHeader>
+                          <TableBody>
+                            {!!prediction.predicted_winner_name && (
+                              <TableRow>
+                                <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
+                                  Pemenang
+                                </TableCell>
+                                <TableCell className="py-1 pl-0">
+                                  {prediction.predicted_winner_name}
+                                </TableCell>
+                              </TableRow>
+                            )}
+                            {prediction.predicted_total_goals !== null && (
+                              <TableRow>
+                                <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
+                                  Jumlah Gol
+                                </TableCell>
+                                <TableCell className="py-1 pl-0">
+                                  {prediction.predicted_total_goals}
+                                </TableCell>
+                              </TableRow>
+                            )}
+                            {!!prediction.predicted_score && (
+                              <TableRow>
+                                <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
+                                  Skor
+                                </TableCell>
+                                <TableCell className="py-1 pl-0">
+                                  {prediction.predicted_score}
+                                </TableCell>
+                              </TableRow>
+                            )}
+                            {!!prediction.predicted_first_team_to_score_name && (
+                              <TableRow>
+                                <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
+                                  Gol Pertama (Negara)
+                                </TableCell>
+                                <TableCell className="py-1 pl-0">
+                                  {
+                                    prediction.predicted_first_team_to_score_name
+                                  }
+                                </TableCell>
+                              </TableRow>
+                            )}
+                            {!!prediction.predicted_first_player_to_score && (
+                              <TableRow>
+                                <TableCell className="py-1 pl-0 max-w-1/2 whitespace-normal">
+                                  Gol Pertama (Pemain)
+                                </TableCell>
+                                <TableCell className="py-1 pl-0">
+                                  {prediction.predicted_first_player_to_score}
+                                </TableCell>
+                              </TableRow>
+                            )}
+                          </TableBody>
+                        </Table>
+                      )}
                     </CollapsibleContent>
                   </Collapsible>
                 ))}
